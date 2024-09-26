633f83f4b584aa8e6f00f626d7c2fcbe950f63c4
varunnallam <nallamvarun6@gmail.com>
2024-09-26 20:27:15 +0530

    Add files via upload

---
+++ 26.03.py
@@ -0,0 +1,6 @@
+for numbers which are multiples of both three and five, print "Fizzbuzz "
+for i in range (1,100+1):
+    if(i%3==0 and i%5==0):
+        print("fizzbuzz")
+    else:
+        print(i)
