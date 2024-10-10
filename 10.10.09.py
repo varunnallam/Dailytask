037e930211859e35d26fa529568608c988f355db
varunnallam <nallamvarun6@gmail.com>
2024-10-10 20:58:14 +0530

    Add files via upload

---
+++ 10.10.09.py
@@ -0,0 +1,5 @@
+"Write a Python program to remove the last element of a list using pop() and print the updated list."
+numbers = [1, 2, 3, 4, 5]
+print("Initial list:", numbers)
+numbers.pop()
+print("Updated list:", numbers)
