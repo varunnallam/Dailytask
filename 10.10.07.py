9d923160ef21d228f7e4070cfd1be7631b3c9d6a
varunnallam <nallamvarun6@gmail.com>
2024-10-10 20:57:25 +0530

    Add files via upload

---
+++ 10.10.07.py
@@ -0,0 +1,7 @@
+"Extends the list with another list of numbers."
+numbers = [1, 2, 3, 4, 5]
+print( numbers)
+# Another list of numbers
+more_numbers = [6, 7, 8, 9]
+numbers.extend(more_numbers)
+print(numbers)
