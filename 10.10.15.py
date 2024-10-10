6261025ce7c618cf9f603c5a325b47b7d2476235
varunnallam <nallamvarun6@gmail.com>
2024-10-10 21:03:29 +0530

    Add files via upload

---
+++ 10.10.15.py
@@ -0,0 +1,4 @@
+"Given a list of numbers, write a Python program to create a new list where each element is doubled (i.e., each element is multiplied by 2)."
+numbers = [1, 2, 3, 4, 5]
+doubled_numbers = [num * 2 for num in numbers]
+print(doubled_numbers)  # Output: [2, 4, 6, 8, 10]
