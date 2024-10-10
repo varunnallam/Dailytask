4f936f23dfab5939b4108ae4e04ee92fd94d97a7
varunnallam <nallamvarun6@gmail.com>
2024-10-10 20:54:41 +0530

    Add files via upload

---
+++ 10.10.02.py
@@ -0,0 +1,6 @@
+"Extends the list with another list of numbers."
+"Create a list of strings containing the names of 5 fruits. Access and print the second and fourth elements using indexing."
+
+fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+print("The second fruit is:", fruits[1])
+print("The fourth fruit is:", fruits[3])
