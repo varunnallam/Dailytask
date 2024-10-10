d3c66f577fb947570c39764a2d71880cdb4648a3
varunnallam <nallamvarun6@gmail.com>
2024-10-10 20:54:07 +0530

    Add files via upload

---
+++ 10.10.01.py
@@ -0,0 +1,6 @@
+" Basic List Operations:"
+" Write a Python program to create a list of 5 integers and print the sum of all elements in the list"
+
+numbers = [1,2,3,4,5]
+total = sum(numbers)
+print("The sum of all elements in the list is:", total)
