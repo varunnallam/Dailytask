297665266fda1bd594fd5769c05f560d3c0b1b63
varunnallam <nallamvarun6@gmail.com>
2024-10-14 18:27:24 +0530

    Add files via upload

---
+++ 14.10.002.py
@@ -0,0 +1,14 @@
+"Write a program that checks if a number is prime. A prime number is a number greater than 1 that is only divisible by 1 and itself"
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
